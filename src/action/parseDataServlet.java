7ee7bcfa4cb258b5abdd04af9359ccd5c97c9471
Yifan Jin <jyfmidi@gmail.com>
2017-08-06 22:11:29 +0800

    pytest

--- src/action/parseDataServlet.java
+++ src/action/parseDataServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import py.pyUtility;
 
 
 @WebServlet("/parseDataServlet")
@@ -17,6 +18,19 @@ public class parseDataServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String n1 = request.getParameter("n1");
+        String n2 = request.getParameter("n2");
+        int num1 = Integer.parseInt(n1);
+        int num2 = Integer.parseInt(n2);
+        String context =request.getServletContext().getRealPath("/");
+        String res = "hello";
+        res = pyUtility.pyTest(num1,num2,context);
 
+        //int res =num1+num2;
+        request.setAttribute("n", res);
+        String url = "test.jsp";
+        url = new String(url.getBytes("UTF-8"), "ISO8859-1");
+
+        request.getRequestDispatcher(url).forward(request, response);
     }
 }
